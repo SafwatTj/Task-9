7b6252bb4456901aadcf01991ccd2795790b8b1a
SafwatTj <safwatburhonovtj@gmail.com>
2024-10-28 18:22:19 +0100

    Delete src/Comparator1.java

--- src/Comparator1.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class ComparatorPersonByAge implements Comparator<Person> {
-    @Override
-    public int compare(Person person1, Person person2) {
-        return person1.getAge() - person2.getAge();
-    }
-}
