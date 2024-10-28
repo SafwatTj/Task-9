3fb4e76b645d39bc3a5d3eceda729a0ae49951c5
SafwatTj <safwatburhonovtj@gmail.com>
2024-10-28 18:23:15 +0100

    Delete src/Comparator5.java

--- src/Comparator5.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class ComparatorPersonByWeight implements Comparator<Person> {
-
-    @Override
-    public int compare(Person o1, Person o2) {
-        return Double.compare(o1.getWeight(),o2.getWeight());
-    }
-}
