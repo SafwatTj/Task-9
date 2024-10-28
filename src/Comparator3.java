15bc9df8bb36071de34fe22f0f94bf72ed1e5276
SafwatTj <safwatburhonovtj@gmail.com>
2024-10-28 18:22:49 +0100

    Delete src/Comparator3.java

--- src/Comparator3.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class ComparatorPersonById implements Comparator<Person> {
-
-    @Override
-    public int compare(Person o1, Person o2) {
-        return o1.getId()-o2.getId();
-    }
-}
