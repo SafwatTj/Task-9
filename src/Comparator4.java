bc571700a1c4ab649627946d02d0e5b5cfd4bb9c
SafwatTj <safwatburhonovtj@gmail.com>
2024-10-28 18:23:01 +0100

    Delete src/Comparator4.java

--- src/Comparator4.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class ComparatorPersonByLastName implements Comparator<Person> {
-    @Override
-    public int compare(Person o1, Person o2) {
-        return o1.getlName().compareTo(o2.getlName());
-    }
-}
